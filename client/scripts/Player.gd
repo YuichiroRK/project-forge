17e37f0fbf71ac01b44eee660659b7067a3e141c
YuichiroRK <julichabes1292@gmail.com>
2026-07-15 18:56:44 -0500

    Fix interact angle and add click support

--- client/scripts/Player.gd
+++ client/scripts/Player.gd
@@ -26,11 +26,31 @@ func _physics_process(_delta: float) -> void:
 	move_and_slide()
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("interact"):
+	if event.is_action_pressed("interact") or (event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed):
 		interact()
 
 func interact():
-	if interact_raycast and interact_raycast.is_colliding():
-		var target = interact_raycast.get_collider()
-		if target and target.has_method("on_interact"):
-			target.on_interact(self)
+	var space_state = get_world_2d().direct_space_state
+	var shape = CircleShape2D.new()
+	shape.radius = 60.0 # Radio de interaccion amplio para que no importe el angulo
+	
+	var query = PhysicsShapeQueryParameters2D.new()
+	query.shape = shape
+	query.transform = global_transform
+	# Excluir al propio jugador para que no colisione consigo mismo
+	query.exclude = [self.get_rid()]
+	
+	var result = space_state.intersect_shape(query)
+	var closest_target = null
+	var min_dist = 9999.0
+	
+	for hit in result:
+		var collider = hit.collider
+		if collider and collider.has_method("on_interact"):
+			var dist = global_position.distance_to(collider.global_position)
+			if dist < min_dist:
+				min_dist = dist
+				closest_target = collider
+				
+	if closest_target:
+		closest_target.on_interact(self)
